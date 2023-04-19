6faf0130fd1b378fb58f407d3381edeef8cc467d
raviteja <teja4360@gmail.com>
2023-04-19 16:48:35 +0530

    Random txt

--- Roboshop/mongodb.sh
+++ Roboshop/mongodb.sh
@@ -5,7 +5,7 @@ yum install mongodb-org -y
 
 
 echo -e "\e[36m update mongo listen IP\e[0m"
-sed -i -e 's|127.0.0.1|0.0.0.0' /etc/mongod.conf
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
 echo -e "\e[36m start mongo\e[0m"
 systemctl enable mongod
 systemctl restart mongod
