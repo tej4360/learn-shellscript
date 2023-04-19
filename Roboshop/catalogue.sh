432716113e947936c3cd1034db262a174045779a
raviteja <teja4360@gmail.com>
2023-04-19 17:01:04 +0530

    Random txt

---
+++ Roboshop/catalogue.sh
@@ -0,0 +1,28 @@
+echo -e "\e[36m download nodejs\e[0m"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+echo -e "\e[36m install nodejs\e[0m"
+
+yum install nodejs -y
+echo -e "\e[36m user add\e[0m"
+useradd roboshop
+echo -e "\e[36m create app dir \e[0m"
+mkdir /app
+echo -e "\e[36m download application content\e[0m"
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+cd /app
+echo -e "\e[36m unzip application content \e[0m"
+unzip /tmp/catalogue.zip
+cd /app
+echo -e "\e[36m install dependencies\e[0m"
+npm install
+echo -e "\e[36m copy catologue service\e[0m"
+cp catalogue.service /etc/systemd/system/catalogue.service
+echo -e "\e[36m load catalogue service\e[0m"
+systemctl daemon-reload
+systemctl enable catalogue
+systemctl start catalogue
+echo -e "\e[36m install mongo\e[0m"
+yum install mongodb-org-shell -y
+echo -e "\e[36m load schema\e[0m"
+mongo --host MONGODB-SERVER-IPADDRESS </app/schema/catalogue.js
