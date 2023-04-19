0f469ac88d0fcecd022d1b782e04fa0aa549c757
raviteja <teja4360@gmail.com>
2023-04-19 16:42:30 +0530

    Random txt

--- Roboshop/mongodb.sh
+++ Roboshop/mongodb.sh
@@ -2,10 +2,10 @@ echo -e "\e[36m Copy mongo repo\e[0m"
 cp Roboshop/mongo.repo /etc/yum.repos.d/mongodb.repo
 echo -e "\e[36minstall mongodb\e[0m"
 yum install mongodb-org -y
-echo -e "\e[36m start mongodb\e[0m"
-systemctl enable mongod
-systemctl start mongod
+
+
 echo -e "\e[36m update mongo listen IP\e[0m"
-sed -i -e 's|127.0.0.1|0.0.0.0' /root/etc/mongo.conf
-echo -e "\e[36m restart mongo\e[0m"
+sed -i -e 's|127.0.0.1|0.0.0.0' /etc/mongo.conf
+echo -e "\e[36m start mongo\e[0m"
+systemctl enable mongod
 systemctl restart mongod
