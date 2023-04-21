fab5316f1ee8501b5e62487848eb31448e02df0c
raviteja <teja4360@gmail.com>
2023-04-21 11:01:18 +0530

    Random txt

--- Roboshop/common.sh
+++ Roboshop/common.sh
@@ -4,9 +4,9 @@ print_head(){
 
 fun_stat_check() {
   if [ $1 -eq 0 ]; then
-      print_head ">>>>>Success<<<<<<"
+      echo -e "\e[35m >>>>>Success<<<<<< \e[0m"
   else
-      print_head "******Failure******"
+      echo -e "\e[35m ******Failure****** \e[0m"
       exit 1
   fi
 }
