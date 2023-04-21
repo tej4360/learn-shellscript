10c7afb9662ecc109ca5127688724030980519ac
raviteja <teja4360@gmail.com>
2023-04-21 10:52:15 +0530

    Random txt

--- Roboshop/catalogue.sh
+++ Roboshop/catalogue.sh
@@ -1,30 +1,40 @@
 source /learnshell/Roboshop/common.sh
-rm -rf /app/roboshop_log
+#rm -rf /app/roboshop_log
 print_head "<<<Setuop Node JS>>>>"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/app/roboshop_log
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+fun_stat_check $?
 print_head "<<<install nodejs>>>"
-
 yum install nodejs -y &>>/app/roboshop_log
+fun_stat_check $?
 print_head "<<<Add application user roboshop>>>"
 useradd roboshop &>>/app/roboshop_log
 rm -rf /app
+fun_stat_check $?
 print_head "Create app dir"
 mkdir /app
+fun_stat_check $?
 print_head "download application content"
 curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
 unzip /tmp/catalogue.zip
+fun_stat_check $?
 print_head  "install dependencies"
-npm install &>>/app/roboshop_log
+npm install
+fun_stat_check $?
 cd /learnshell
 print_head "install mongo"
 cp Roboshop/mongo.repo /etc/yum.repos.d/mongodb.repo
+fun_stat_check $?
 yum install mongodb-org-shell -y
+fun_stat_check $?
 print_head "load schema"
 mongo --host mongo.rtdevopspract.online </app/schema/catalogue.js
+fun_stat_check $?
 print_head "copy catologue service"
 cp Roboshop/catalogue.service /etc/systemd/system/catalogue.service
+fun_stat_check $?
 print_head  "load catalogue service"
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl start catalogue
+fun_stat_check $?
--- Roboshop/common.sh
+++ Roboshop/common.sh
@@ -1,3 +1,11 @@
 print_head(){
   echo -e "\e[36m $1 \e[0m"
 }
+
+fun_stat_check() {
+  if [$1 -eq 0] then
+      print_head ">>>>>Success<<<<<<"
+  else
+      print_head "******Failure******"
+      exit
+}
