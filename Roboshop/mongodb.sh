631b2ca0fe03d8f3b9e860749017f9356a57c179
raviteja <teja4360@gmail.com>
2023-04-21 14:55:01 +0530

    Random txt

--- Roboshop/mongodb.sh
+++ Roboshop/mongodb.sh
@@ -1,14 +1,14 @@
 source /learnshell/Roboshop/common.sh
 print_head "Copy mongo repo"
-cp Roboshop/mongo.repo /etc/yum.repos.d/mongodb.repo
+cp Roboshop/mongo.repo /etc/yum.repos.d/mongodb.repo &>>/etc/roboshop_log
 fun_stat_check $?
 print_head "install mongodb"
-yum install mongodb-org -y
+yum install mongodb-org -y &>>/etc/roboshop_log
 fun_stat_check $?
 print_head "update mongo listen IP"
-sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf &>>/etc/roboshop_log
 fun_stat_check $?
 print_head "start mongo"
-systemctl enable mongod
-systemctl restart mongod
+systemctl enable mongod &>>/etc/roboshop_log
+systemctl restart mongod &>>/etc/roboshop_log
 fun_stat_check $?
