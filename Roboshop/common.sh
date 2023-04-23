42541cf25bb2d81bf9e8f82698ef14cb96e0401e
raviteja <teja4360@gmail.com>
2023-04-23 21:54:17 +0530

    Random txt

--- Roboshop/common.sh
+++ Roboshop/common.sh
@@ -10,7 +10,7 @@ fun_stat_check() {
       echo -e "\e[35m >>>>>Success<<<<<< \e[0m"
   else
       echo -e "\e[35m ******Failure****** \e[0m"
-      print_head "*** Please refer /tmp/roboshop_log file for detail report *****"
+      print_head "*** Please refer /etc/roboshop_log file for detail report *****"
       exit 1
   fi
 }
