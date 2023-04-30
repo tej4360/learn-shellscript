70e278c32c220e45d3d17dee55aefc6ae793eba5
raviteja <teja4360@gmail.com>
2023-04-30 16:26:00 +0530

    Random txt

--- Roboshop/frontend.sh
+++ Roboshop/frontend.sh
@@ -2,8 +2,8 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 component=frontend
-rm -rf $log_path
 source $script_path/common.sh
+rm -rf $log_path
 print_head "install NGINX"
 yum install nginx -y &>>$log_path
 fun_stat_check $?
