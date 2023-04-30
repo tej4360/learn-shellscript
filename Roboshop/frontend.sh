ca64df47367f0542652db9308bf055c5158cbba9
raviteja <teja4360@gmail.com>
2023-04-30 16:30:44 +0530

    Random txt

--- Roboshop/frontend.sh
+++ Roboshop/frontend.sh
@@ -5,26 +5,26 @@ source ${script_path}/common.sh
 
 print_head "Install Nginx"
 yum install nginx -y &>>$log_path
-func_stat_check $?
+fun_stat_check $?
 
 print_head "Copy roboshop Config file"
 cp roboshop.conf /etc/nginx/default.d/roboshop.conf &>>$log_path
-func_stat_check $?
+fun_stat_check $?
 
 print_head "Clean Old App content"
 rm -rf /usr/share/nginx/html/* &>>$log_path
-func_stat_check $?
+fun_stat_check $?
 
 print_head "Download App Content"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>$log_path
-func_stat_check $?
+fun_stat_check $?
 
 print_head "Extracting App Content"
 cd /usr/share/nginx/html &>>$log_path
 unzip /tmp/frontend.zip &>>$log_path
-func_stat_check $?
+fun_stat_check $?
 
 print_head "Start Nginx"
 systemctl enable nginx &>>$log_path
 systemctl restart nginx &>>$log_path
-func_stat_check $?
+fun_stat_check $?
