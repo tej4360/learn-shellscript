5bc575816a69be6630dc4156ed29552910aa0a42
raviteja <teja4360@gmail.com>
2023-04-30 16:24:02 +0530

    Random txt

--- Roboshop/frontend.sh
+++ Roboshop/frontend.sh
@@ -1,24 +1,12 @@
 ##frontend shell automation
 script=$(realpath "$0")
 script_path=$(dirname "$script")
+component=frontend
 rm -rf $log_path
 source $script_path/common.sh
 print_head "install NGINX"
 yum install nginx -y &>>$log_path
 fun_stat_check $?
-print_head "remove application content"
-rm -rf /usr/share/nginx/html/* &>>$log_path
-fun_stat_check $?
-print_head "download application content"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>$log_path
-fun_stat_check $?
-cd /usr/share/nginx/html
-print_head  "unzip front end"
-unzip /tmp/frontend.zip &>>$log_path
-fun_stat_check $?
-print_head "copy config file"
-cp $script_path/roboshop.conf /etc/nginx/default.d/roboshop.conf &>>$log_path
-systemctl enable nginx &>>$log_path
-systemctl restart nginx &>>$log_path
-fun_stat_check $?
+fun_get_app_content
+func_systemd_setup
 
