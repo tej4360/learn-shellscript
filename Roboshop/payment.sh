5cc66ea8f8edc6af42beca86ca31894b27d1d9b2
raviteja <teja4360@gmail.com>
2023-05-01 11:18:58 +0530

    Random txt

--- Roboshop/payment.sh
+++ Roboshop/payment.sh
@@ -10,17 +10,17 @@ fi
 
   print_head "Install Python"
   yum install python36 gcc python3-devel -y &>>$log_path
-  func_stat_check $?
+  fun_stat_check $?
 
   fun_check_user
   fun_get_app_content
 
   print_head "Install Python Dependencies"
   pip3.6 install -r requirements.txt &>>$log_path
-  func_stat_check $?
+  fun_stat_check $?
 
   print_head "Update Passwords in System Service file"
   sed -i -e "s|rabbitmq_appuser_password|${rabbitmq_appuser_password}|" ${script_path}/payment.service &>>$log_path
-  func_stat_check $?
+  fun_stat_check $?
 
   func_systemd_setup
