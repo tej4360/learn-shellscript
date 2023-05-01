1c7f3fbb9909d169399a3f894e753a69cc10a731
raviteja <teja4360@gmail.com>
2023-05-01 11:15:53 +0530

    Random txt

--- Roboshop/rabbitmq.sh
+++ Roboshop/rabbitmq.sh
@@ -11,22 +11,22 @@ fi
 
 print_head "setup erlang repos"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>>$log_path
-func_stat_check $?
+fun_stat_check $?
 
 print_head "Setup RabbitMQ Repos"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>$log_path
-func_stat_check $?
+fun_stat_check $?
 
 print_head "Install ErLang & RabbitMQ"
 yum install erlang rabbitmq-server -y &>>$log_path
-func_stat_check $?
+fun_stat_check $?
 
 print_head "Start RabbitMQ Service"
 systemctl enable rabbitmq-server &>>$log_path
 systemctl restart rabbitmq-server &>>$log_path
-func_stat_check $?
+fun_stat_check $?
 
 func_print_head "Add Application User in RabbtiMQ"
-rabbitmqctl add_user roboshop ${rabbitmq_appuser_password} &>>$log_file
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$log_file
-func_stat_check $?
+rabbitmqctl add_user roboshop ${rabbitmq_appuser_password} &>>$log_path
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$log_path
+fun_stat_check $?
