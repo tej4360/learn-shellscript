e77146ad2702281fbd62f77815ced537950fc9c9
raviteja <teja4360@gmail.com>
2023-05-03 21:26:53 +0530

    Random txt

--- Terraform/sample.tf
+++ Terraform/sample.tf
@@ -1,10 +1,10 @@
-variable "sample"{
-    default {
-        number=10
-        string="abc"
+variable "sample" {
+    default = {
+        number = 10
+        string = "abc"
     }
 }
 
-output "sample"{
-        value="value of map var = ${var.string}
+output "sample" {
+        value = "value of map var = ${var.string}"
 }
