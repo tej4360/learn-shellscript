6aeb1b14ce893c8b22aeb3b0e9267f80fcc32f23
raviteja <teja4360@gmail.com>
2023-05-04 18:41:08 +0530

    Random txt

--- Terraform/sample.tf
+++ Terraform/sample.tf
@@ -1,9 +1,4 @@
-resource "aws_spot_instance_request" "cheap_worker" {
-  ami           = "ami-0089b8e98cd95257d"
-  spot_price    = "0.03"
-  instance_type = "c4.xlarge"
-
-  tags = {
-    Name = "CheapWorker"
-  }
+resource "aws_instance" "sample instance" {
+  ami                  = "ami-0089b8e98cd95257d"
+  instance_type        = "t3.micro"
 }
