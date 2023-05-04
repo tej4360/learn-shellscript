355c616f7e792659cf9f0c08a3640c2d4febb3cb
raviteja <teja4360@gmail.com>
2023-05-04 18:50:09 +0530

    Random txt

--- Terraform/sample.tf
+++ Terraform/sample.tf
@@ -1,8 +1,8 @@
-resource "aws_instance" "sample_instance" {
-  ami                  = "ami-0089b8e98cd95257d"
-  instance_type        = "t3.micro"
+resource "aws_instance" "web" {
+  ami           = "ami-0089b8e98cd95257d"
+  instance_type = "t3.micro"
 
-    tags {
-        Name = "sample_instance"
-    }
+  tags = {
+    Name = "HelloWorld"
+  }
 }
