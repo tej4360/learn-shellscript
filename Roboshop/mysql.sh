35f47ce11ed40b94cca1ec4b05641f0b8ba15008
raviteja <teja4360@gmail.com>
2023-05-20 21:46:38 +0530

    Random txt

--- Roboshop/mysql.sh
+++ Roboshop/mysql.sh
@@ -11,21 +11,21 @@ fi
 
 print_head "Disable MySQL 8 Version"
 dnf module disable mysql -y &>>$log_path
-func_stat_check $?
+fun_stat_check $?
 
 print_head "Copy MySQL Repo File"
 cp ${script_path}/mysql.repo /etc/yum.repos.d/mysql.repo &>>$log_path
-func_stat_check $?
+fun_stat_check $?
 
 print_head "Install MySQL"
 yum install mysql-community-server -y &>>$log_path
-func_stat_check $?
+fun_stat_check $?
 
 print_head "Start MySQL"
 systemctl enable mysqld &>>$log_path
 systemctl restart mysqld &>>$log_path
-func_stat_check $?
+fun_stat_check $?
 
 print_head "Reset MySQL Password"
 mysql_secure_installation --set-root-pass $mysql_root_password &>>$log_path
-func_stat_check $?
+fun_stat_check $?
