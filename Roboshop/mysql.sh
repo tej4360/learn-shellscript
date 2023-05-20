cad0a7b2fe5c26912d0d41fe60e17751901b9fee
raviteja <teja4360@gmail.com>
2023-05-20 17:26:04 +0530

    Random txt

--- Roboshop/mysql.sh
+++ Roboshop/mysql.sh
@@ -18,7 +18,7 @@ fun_stat_check $?
 print_head "install mysql"
 yum install mysql-community-server -y &>>$log_path
 fun_stat_check $?
-print_head "Reset MySQL Password"
+print_head "Reset MySQL Password to : ${mysql_root_password}"
 mysql_secure_installation --set-root-pass $mysql_root_password -y
 fun_stat_check $?
 print_head "Start MySQL"
