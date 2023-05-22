172933cd1beee140dc4327c5dff66e7cb2644b29
root <root@ip-172-31-8-73.ec2.internal>
2023-05-22 10:32:09 +0000

    Changes made by Raghu

--- Roboshop/cart.sh
+++ Roboshop/cart.sh
@@ -3,12 +3,34 @@ script_path=$(dirname "$script")
 source $script_path/common.sh
 component=cart
 rm -rf log_path
-print_head "download node js"
+print_head "<<<Setup Node JS>>>>"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>log_path
 fun_stat_check $?
-print_head "install node js"
-yum install nodejs -y &>>log_path
+print_head "<<<install nodejs>>>"
+yum install nodejs -y &>>log_path &>>log_path
 fun_stat_check $?
-fun_check_user
-fun_get_app_content
-func_systemd_setup
+print_head "<<<Add application user roboshop>>>"
+useradd roboshop &>>log_path
+print_head "remove /app dir"
+rm -rf /app &>>log_path
+fun_stat_check $?
+print_head "Create app dir"
+mkdir /app &>>log_path
+fun_stat_check $?
+print_head "download application content"
+curl -L -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>>log_path
+cd /app
+unzip /tmp/$component.zip &>>log_path
+fun_stat_check $?
+print_head  "install dependencies"
+npm install &>>log_path
+fun_stat_check $?
+cd /learnshell
+print_head "copy cart service"
+cp $script_path/$component.service /etc/systemd/system/$component.service &>>log_path
+fun_stat_check $?
+print_head  "load cart service"
+systemctl daemon-reload &>>log_path
+systemctl enable $component &>>log_path
+systemctl start $component &>>$log_path
+fun_stat_check $?
